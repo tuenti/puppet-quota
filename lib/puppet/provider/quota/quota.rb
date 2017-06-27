a6919a59a65242d3ec970fbb659a13b05891eabe
Jose Plana <jplana@tuenti.com>
2017-06-27 16:58:36 +0200

    Remove quotes

--- lib/puppet/provider/quota/quota.rb
+++ lib/puppet/provider/quota/quota.rb
@@ -5,13 +5,13 @@ Puppet::Type.type(:quota).provide(:quota) do
   commands :repquota => "quota"
 
   def create
-    setquota("-u:#{@resource[:name]}",'-b','-q',"#{@resource[:block_soft_limit]}",'-l',"#{@resource[:block_hard_limit]}","#{@resource[:filesystem]}")
-    setquota("-u:#{@resource[:name]}",'-i','-q',"#{@resource[:inode_soft_limit]}",'-l',"#{@resource[:inode_hard_limit]}","#{@resource[:filesystem]}")
+    setquota("-u", "#{@resource[:name]}",'-b','-q',"#{@resource[:block_soft_limit]}",'-l',"#{@resource[:block_hard_limit]}","#{@resource[:filesystem]}")
+    setquota("-u", "#{@resource[:name]}",'-i','-q',"#{@resource[:inode_soft_limit]}",'-l',"#{@resource[:inode_hard_limit]}","#{@resource[:filesystem]}")
   end
 
   def destroy
-    setquota("-u:#{@resource[:name]}",'-b','-q',0,'-l',0,"#{@resource[:filesystem]}")
-    setquota("-u:#{@resource[:name]}",'-i','-q',0,'-l',0,"#{@resource[:filesystem]}")
+    setquota("-u", "#{@resource[:name]}",'-b','-q',0,'-l',0,"#{@resource[:filesystem]}")
+    setquota("-u", "#{@resource[:name]}",'-i','-q',0,'-l',0,"#{@resource[:filesystem]}")
   end
 
   def exists?
